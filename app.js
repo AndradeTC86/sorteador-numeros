602cd84b52059094ff0648314d95129d9db57f95
Thiago Cesar Andrade <tcesaran@emeal.nttdata.com>
2024-09-17 17:08:34 -0300

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear(){
+    let quantidade = document.querySelector()
+    let numInicial = document.querySelector()
+    let numFinal = document.querySelector()
+
+}
+
+function reiniciar(){
+
+}
