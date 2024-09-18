0e55cecd2ba032bd811ce2eb9c651be351460b2e
Thiago Cesar <andrade-tc@uol.com.br>
2024-09-17 21:26:02 -0300

    Finalizando curso

--- app.js
+++ app.js
@@ -3,22 +3,35 @@ mensagemIncial()
 function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value)
     let numInicial = parseInt(document.getElementById('de').value)
-    let numFinal = parseFloat(document.getElementById('ate').value)
+    let numFinal = parseInt(document.getElementById('ate').value)
 
     let sorteados = []
     let numero
-
-    for (let i = 0; i < quantidade; i++){
-        numero = sortearNumero(numInicial, numFinal)
-        while(sorteados.includes(numero)){
-            numero = sortearNumero(numInicial, numFinal)
-        }
-        sorteados.push(numero)
+    
+    if (numInicial >= numFinal) {
+        alert("O número final deve ser maior que o número inicial.");
     }
-    let mensagem = `Números sorteados:  ${sorteados}`
-    exibirTextoEmTela('resultado', mensagem)
-    mudarClasseBotao()
-}
+    
+    if (quantidade > (numFinal - numInicial + 1)) {
+        alert("A quantidade de números a ser sorteada deve ser menor ou igual ao intervalo disponível.");
+    }
+    
+    else {
+        for (let i = 0; i < quantidade; i++) {
+            numero = sortearNumero(numInicial, numFinal)
+            while (sorteados.includes(numero)) {
+                numero = sortearNumero(numInicial, numFinal)
+            }
+            sorteados.push(numero)
+        }
+
+        // Exibir o resultado
+        let mensagem = `Números sorteados: ${sorteados}`
+        exibirTextoEmTela('resultado', mensagem)
+        mudarClasseBotao()
+    }
+}   
+
 
 function sortearNumero(min, max){
     return Math.floor(Math.random() * (max -min + 1) + min)
@@ -45,7 +58,7 @@ function limparCampos(){
     campos.forEach(campo => campo.value = ' ')
 }
 
-function mudarClasseBotao(atual, novo){
+function mudarClasseBotao(){
     let botao = document.getElementById('btn-reiniciar')
     if (botao.classList.contains('container__botao-desabilitado')) {
         botao.classList.remove('container__botao-desabilitado');
